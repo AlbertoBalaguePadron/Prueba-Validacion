45e1bb628f8eb5723637ec49249486a1fcf2259d
Alberto <alberto01bp@gmail.com>
2022-11-14 17:53:51 +0000

    Feature: Agrego un modal el cual sirve para agregar los datos para la lsita de transacciones, tengo una targeta que muestra la cantidad de dinero

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, Modal, StyleSheet, Text, View } from "react-native";
+import Money from "./components/Money";
 
 export default function App() {
+  const [dinero, setDinero] = useState("0");
+  const [mostrar, setMostrar] = useState(false);
+
+  
+  const changeTextHandler = (value) => {
+    setDinero(value);
+}
+
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Money DineroActual={dinero} />
+      <Text>Posición Lista </Text>
+
+      <Text
+        style={styles.boton}
+        onPress={() => {
+          setMostrar(!mostrar);
+        }}
+      >
+        +
+      </Text>
+
+      <Modal visible={mostrar} animationType={"fade"} transparent={false}>
+
+      <TextInput style={ styles.imputTransac } 
+                pla ceholder='Introduzca la transación' 
+                keyboardType="numeric"
+                onChangeText={ changeTextHandler } 
+                value={ dinero }/>
+
+        <Button
+          title="Cerrar"
+          onPress={() => {
+            setMostrar(false);
+          }}
+        />
+      </Modal>
     </View>
   );
 }
@@ -12,8 +47,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#6f74dd",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  modalbody: {
+    backgroundColor: "#6f74dd",
+    color: "white,",
+    width: "70%",
+    height: 30,
+    marginTop: 20,
+  },
+  boton: {
+    flexDirection: 'column',
+    flex: 1,
+    justifyContent: "center",
+    textAlign: "center",
+    position: "absolute",
+    marginBottom: 10, 
+    bottom: 50,
+    right: 20,
+    width: 40,
+    height: 40,
+    backgroundColor: "white",
+    color: "black",
+    borderRadius: 20,
+  },
+  imputTransac:{
+    flex: 4,
+    color: 'black'
   },
 });
---
+++ components/Money.jsx
@@ -0,0 +1,38 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Money = ({DineroActual}) => {
+    return (
+        <View style={ styles.containerProcut }>
+            {
+                DineroActual >= 0 ? (
+                    <Text style={ styles.Positivo }>{DineroActual}</Text>
+                ):(
+                    <Text style={ styles.Negativo }>{DineroActual}</Text>
+                )
+            }
+        </View>
+)}
+
+
+const styles = StyleSheet.create({
+    containerProcut: {
+        alignItems: 'center',
+        backgroundColor: "#00227b",
+        flexDirection: 'row',
+        height: 100,
+        justifyContent: 'space-between',
+        padding: 10,
+        marginTop: 30, 
+        marginBottom: 30,
+        width: '100%',
+        radius: 1000,
+    },
+    Positivo: {
+        color: 'white',
+    },  
+    Negativo: {
+        color: 'red',
+    },
+});
+
+export default Money; 
